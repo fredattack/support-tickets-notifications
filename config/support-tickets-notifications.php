7949be5ef2424e238233bbbf27019184efa5d43b
fredattack <Fred01072015!>
2021-10-01 08:26:08 +0200

    sending slack begin

--- config/support-tickets-notifications.php
+++ config/support-tickets-notifications.php
@@ -2,5 +2,6 @@
 // config for Fredattack/SupportTicketsNotifications
     return [
         "default_channel" => env ( 'DEFAULT_SUPPORT_NOTIFICATION_CHANNEL' , 'slack' ) ,
+        "slack_channel" => env ( 'ADDF_SUPPORT_NOTIFICATION_SLACK_CHANNEL' , 'slack' ) ,
         "user_class" => 'App\Models\User' ,
     ];
--- src/Listeners/NotifyADDFTeam.php
+++ src/Listeners/NotifyADDFTeam.php
@@ -6,13 +6,19 @@
     
     use Fredattack\SupportTicketsNotifications\Events\TicketCreated;
     use Fredattack\SupportTicketsNotifications\Mail\NewTicketEmail;
+    use Fredattack\SupportTicketsNotifications\Notification\SlackNotification;
     use Mail;
-
+    
     class NotifyADDFTeam
     {
-        public function handle(TicketCreated $event) {
-                ray('NotifyADDFTeam',$event->ticket)->red();
-            Mail ::to ('fred.moras@addf.be')  -> send ( new NewTicketEmail($event->ticket));
+        public function handle ( TicketCreated $event )
+        {
+            ray ( 'NotifyADDFTeam' , $event -> ticket ) -> red ();
+            Mail ::to ( 'fred.moras@addf.be' ) -> send ( new NewTicketEmail( $event -> ticket ) );
+            
+            config ( 'support-tickets-notifications.user_class' ) ::Role ( 'super-admin' )
+                -> first ()
+                -> notify ( new slackNotification( $event -> ticket ,\Auth::user ()) );
             
         }
     }
