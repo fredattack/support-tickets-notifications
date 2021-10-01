7d12d747fed25e7e48ee4e24405dbabba32bd095
fredattack <fredattack@users.noreply.github.com>
2021-10-01 06:26:59 +0000

    Fix styling

--- src/Listeners/NotifyADDFTeam.php
+++ src/Listeners/NotifyADDFTeam.php
@@ -1,24 +1,21 @@
 <?php
-    
-    
+
     namespace Fredattack\SupportTicketsNotifications\Listeners;
-    
-    
+
     use Fredattack\SupportTicketsNotifications\Events\TicketCreated;
     use Fredattack\SupportTicketsNotifications\Mail\NewTicketEmail;
     use Fredattack\SupportTicketsNotifications\Notification\SlackNotification;
     use Mail;
-    
+
     class NotifyADDFTeam
     {
-        public function handle ( TicketCreated $event )
+        public function handle(TicketCreated $event)
         {
-            ray ( 'NotifyADDFTeam' , $event -> ticket ) -> red ();
-            Mail ::to ( 'fred.moras@addf.be' ) -> send ( new NewTicketEmail( $event -> ticket ) );
-            
-            config ( 'support-tickets-notifications.user_class' ) ::Role ( 'super-admin' )
-                -> first ()
-                -> notify ( new slackNotification( $event -> ticket ,\Auth::user ()) );
-            
+            ray('NotifyADDFTeam', $event -> ticket) -> red();
+            Mail ::to('fred.moras@addf.be') -> send(new NewTicketEmail($event -> ticket));
+
+            config('support-tickets-notifications.user_class') ::Role('super-admin')
+                -> first()
+                -> notify(new slackNotification($event -> ticket, \Auth::user()));
         }
     }
--- src/Notification/SlackNotification.php
+++ src/Notification/SlackNotification.php
@@ -1,46 +1,39 @@
 <?php
-    
-    
+
     namespace Fredattack\SupportTicketsNotifications\Notification;
-    
-    
-    
-    
+
     use Fredattack\SupportTicketsNotifications\Models\TicketSupport;
     use Illuminate\Bus\Queueable;
-    use Illuminate\Contracts\Queue\ShouldQueue;
-    use Illuminate\Notifications\Messages\MailMessage;
-    use Illuminate\Notifications\Notification;
     use Illuminate\Notifications\Messages\SlackMessage;
-    
+    use Illuminate\Notifications\Notification;
+
     class SlackNotification extends Notification
     {
         use Queueable;
-        
+
         private $user;
         private TicketSupport $ticket_support;
-    
-        public function __construct ( TicketSupport $ticket_support,$user)
+
+        public function __construct(TicketSupport $ticket_support, $user)
         {
             $this -> user = $user;
             $this -> ticket_support = $ticket_support;
         }
-        
-        public function via ( $notifiable )
+
+        public function via($notifiable)
         {
             return [ 'slack' ];
         }
-        
-        public function toSlack ( $notifiable )
+
+        public function toSlack($notifiable)
         {
             return ( new SlackMessage() )
-                -> content ( $this -> getMessageContent () )
-                ->warning ()
-                ->from ( $this -> user -> full_name);
-               
+                -> content($this -> getMessageContent())
+                ->warning()
+                ->from($this -> user -> full_name);
         }
-    
-        public function getMessageContent () : string
+
+        public function getMessageContent(): string
         {
             return 'nuestro primer mensaje con Slack<br/><a href="'.config('app.url').'/support-ticket/ticket/'.$this->ticket_support->id.'">See Message</a>';
         }
